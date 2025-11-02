e4ecf076196c67b6c309877cc3eec184319b5638
Thanh bình <binhnt1509204@gmail.com>
2025-11-03 00:41:29 +0700

    init 3-layer

---
+++ Repository/DAOs/FunewsManagementSystemDbContext.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Repository.DAOs;
+
+public partial class FunewsManagementSystemDbContext : DbContext
+{
+    public FunewsManagementSystemDbContext()
+    {
+    }
+
+    public FunewsManagementSystemDbContext(DbContextOptions<FunewsManagementSystemDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<NewsArticle> NewsArticles { get; set; }
+
+    public virtual DbSet<SystemAccount> SystemAccounts { get; set; }
+
+    public virtual DbSet<Tag> Tags { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(GetConnectionString());
+
+    private string GetConnectionString()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())   // đường dẫn gốc
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return config.GetConnectionString("DefaultConnection");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Category__19093A2B2BF89BE9");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryDesciption).HasMaxLength(500);
+            entity.Property(e => e.CategoryName).HasMaxLength(100);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.ParentCategoryId).HasColumnName("ParentCategoryID");
+
+            entity.HasOne(d => d.ParentCategory).WithMany(p => p.InverseParentCategory)
+                .HasForeignKey(d => d.ParentCategoryId)
+                .HasConstraintName("FK_Category_ParentCategory");
+        });
+
+        modelBuilder.Entity<NewsArticle>(entity =>
+        {
+            entity.HasKey(e => e.NewsArticleId).HasName("PK__NewsArti__4CD0926C1188F313");
+
+            entity.ToTable("NewsArticle");
+
+            entity.Property(e => e.NewsArticleId).HasColumnName("NewsArticleID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CreatedById).HasColumnName("CreatedByID");
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Headline).HasMaxLength(500);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.NewsContent).HasColumnType("ntext");
+            entity.Property(e => e.NewsSource).HasMaxLength(200);
+            entity.Property(e => e.NewsTitle).HasMaxLength(200);
+            entity.Property(e => e.UpdatedById).HasColumnName("UpdatedByID");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.NewsArticles)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_NewsArticle_Category");
+
+            entity.HasOne(d => d.CreatedBy).WithMany(p => p.NewsArticleCreatedBies)
+                .HasForeignKey(d => d.CreatedById)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_NewsArticle_CreatedBy");
+
+            entity.HasOne(d => d.UpdatedBy).WithMany(p => p.NewsArticleUpdatedBies)
+                .HasForeignKey(d => d.UpdatedById)
+                .HasConstraintName("FK_NewsArticle_UpdatedBy");
+
+            entity.HasMany(d => d.Tags).WithMany(p => p.NewsArticles)
+                .UsingEntity<Dictionary<string, object>>(
+                    "NewsTag",
+                    r => r.HasOne<Tag>().WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_NewsTag_Tag"),
+                    l => l.HasOne<NewsArticle>().WithMany()
+                        .HasForeignKey("NewsArticleId")
+                        .HasConstraintName("FK_NewsTag_NewsArticle"),
+                    j =>
+                    {
+                        j.HasKey("NewsArticleId", "TagId").HasName("PK__NewsTag__9A875DC8CC07E8BF");
+                        j.ToTable("NewsTag");
+                        j.IndexerProperty<int>("NewsArticleId").HasColumnName("NewsArticleID");
+                        j.IndexerProperty<int>("TagId").HasColumnName("TagID");
+                    });
+        });
+
+        modelBuilder.Entity<SystemAccount>(entity =>
+        {
+            entity.HasKey(e => e.AccountId).HasName("PK__SystemAc__349DA586CF9176BE");
+
+            entity.ToTable("SystemAccount");
+
+            entity.HasIndex(e => e.AccountEmail, "UQ__SystemAc__FC770D33C1B275B7").IsUnique();
+
+            entity.Property(e => e.AccountId).HasColumnName("AccountID");
+            entity.Property(e => e.AccountEmail)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.AccountName).HasMaxLength(100);
+            entity.Property(e => e.AccountPassword)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Tag>(entity =>
+        {
+            entity.HasKey(e => e.TagId).HasName("PK__Tag__657CFA4C46387EBA");
+
+            entity.ToTable("Tag");
+
+            entity.HasIndex(e => e.TagName, "UQ__Tag__BDE0FD1D8E016D05").IsUnique();
+
+            entity.Property(e => e.TagId).HasColumnName("TagID");
+            entity.Property(e => e.Note).HasMaxLength(255);
+            entity.Property(e => e.TagName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
